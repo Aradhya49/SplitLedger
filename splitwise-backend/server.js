9c5383e5dd07bcbf391596a9bb77e5adf1e0ad03
Aradhya49 <aradhyapriya1102@gmail.com>
2026-05-14 02:25:57 +0530

    Fixed MongoDB connection for production

--- splitwise-backend/server.js
+++ splitwise-backend/server.js
@@ -1,39 +1,33 @@
-require("dotenv").config();
-
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const expenseRoutes = require("./routes/expenses");
-
-const authRoutes =
-  require("./routes/auth");
+const authRoutes = require("./routes/auth");
 
 const app = express();
-mongoose.connect(
-  process.env.MONGO_URI
-)
+
+mongoose.connect(process.env.MONGO_URI)
 
 .then(() => {
-
-  console.log(
-    "MongoDB Connected"
-  );
-
+  console.log("MongoDB Connected");
 })
 
-.catch(err => {
-
+.catch((err) => {
   console.log(err);
-
 });
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/expenses", expenseRoutes);
 app.use("/auth", authRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+
